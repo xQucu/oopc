eb400ed0a96716d6e3664da8306cd2eb8f136e31
xqucu <kucykadrian2004@gmail.com>
2025-12-14 17:58:39 +0100

    feat: add more tests

--- map-template/TestMap.cpp
+++ map-template/TestMap.cpp
@@ -1,16 +1,32 @@
+#include "Book.h"
 #include "Employee.h" // Defines class Employee
 #include "map.h" // Defines template Map<Key, Value>
 #include <iostream>
+#include <string>
 
 typedef unsigned int ID; // Identification number of employee
 typedef Map<ID, Employee> Database; // Database of employees
 
 using namespace std;
+typedef Map<std::string, Book> Library;
 
 void addEmployees(Database& database);
 void modifyEmployees(Database& database);
+void demonstrateLibrary();
+void demonstrateEmployees();
 
 int main()
+{
+    demonstrateEmployees();
+
+    cout << "-----------------------" << endl;
+
+    demonstrateLibrary();
+
+    return 0;
+}
+
+void demonstrateEmployees()
 {
     Database database;
     addEmployees(database);
@@ -27,6 +43,12 @@ int main()
     database = newDatabase; // Update original database
 
     cout << "Database after the assignment:" << endl
+         << database << endl;
+
+    cout << "Removing employee with ID 730505129 (Anna Zaradna)..." << endl;
+    database.remove(730505129);
+
+    cout << "Database after removal:" << endl
          << database << endl;
 };
 
@@ -46,4 +68,48 @@ void modifyEmployees(Database& database)
 
     employeePtr = database.find(761028073); // Find employee using its ID
     employeePtr->age = 29; // Modify the age of employee
+}
+
+void demonstrateLibrary()
+{
+    Library library;
+
+    library.add("The Great Gatsby", Book("F. Scott Fitzgerald", "Fiction", 180, "on the bookshelf"));
+    library.add("1984", Book("George Orwell", "Dystopian Fiction", 328, "on the bookshelf"));
+    library.add("To Kill a Mockingbird", Book("Harper Lee", "Fiction", 281, "borrowed"));
+    library.add("Pride and Prejudice", Book("Jane Austen", "Romance", 432, "on the bookshelf"));
+    library.add("The Catcher in the Rye", Book("J.D. Salinger", "Fiction", 277, "on the bookshelf"));
+    library.add("Animal Farm", Book("George Orwell", "Political Satire", 112, "borrowed"));
+    library.add("Brave New World", Book("Aldous Huxley", "Science Fiction", 268, "on the bookshelf"));
+
+    cout << endl
+         << "Library catalog:" << endl;
+    cout << library << endl;
+
+    Library newLibrary = library;
+
+    cout << "Finding '1984' and changing its status to borrowed..." << endl;
+    Book* bookPtr = library.find("1984");
+    if (bookPtr != nullptr) {
+        cout << "Before: " << *bookPtr << endl;
+        bookPtr->status = "borrowed";
+        cout << "After: " << *bookPtr << endl;
+    } else {
+        cout << "Book not found!" << endl;
+    }
+
+    cout << endl
+         << "Current library catalog:" << endl;
+    cout << library << endl;
+
+    cout << "Backup library catalog (before modifications):" << endl;
+    cout << newLibrary << endl;
+
+    cout << "Searching for 'Harry Potter'..." << endl;
+    bookPtr = library.find("Harry Potter");
+    if (bookPtr != nullptr) {
+        cout << "Found: " << *bookPtr << endl;
+    } else {
+        cout << "Book 'Harry Potter' not found in the library!" << endl;
+    }
 }
